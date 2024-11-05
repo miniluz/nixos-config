1772fe409f9528544fe3d84217c0ecfc600cc3ec
miniluz <javiermelon4fu@gmail.com>
2024-11-05 18:11:57 +0100

    feat: :sparkles: Add ArcWTF to Firefox

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,6 +1,14 @@
 { config, pkgs, lib, osConfig, ... }:
 let
   cfg = config.miniluz.firefox;
+  profile = "miniluz";
+  arcwtf = pkgs.fetchFromGitHub {
+    owner = "KiKaraage";
+    repo = "ArcWTF";
+    # rev = "v1.2-firefox"; # This tag can't be used, since the URL bar has still issues
+    rev = "bb6f2b7ef7e3d201e23d86bf8636e5d0ea4bd68b";
+    hash = "sha256-gyJiIVnyZOYVX6G3m4SSbsb7K9g4zKZWlrHphEIQwsY=";
+  };
 in
 {
   options.miniluz.firefox.enable = lib.mkEnableOption "Enable Firefox.";
@@ -8,18 +16,13 @@ in
   config = lib.mkIf cfg.enable {
     programs.firefox = {
       enable = true;
-      package = pkgs.firefox-beta;
-      profiles.miniluz = {
-        name = "miniluz";
+      profiles.${profile} = {
+        name = profile;
         search.default = "DuckDuckGo";
 
         settings = {
           "extensions.pocket.enabled" = false;
 
-          # "widget.non-native-theme.scrollbar.style" = 3;
-          # "apz.overscroll.enabled" = false;
-
-          # "browser.compactmode.show" = true;
           "browser.toolbars.bookmarks.visibility" = "never";
           "browser.warnOnQuit" = true; # ask for confirmation when closing a window
           "browser.tabs.warnOnClose" = true; # ask for confirmation when closing a window with multiple tabs
@@ -30,6 +33,12 @@ in
           "sidebar.position_start" = true;
           "sidebar.revamp" = true;
           "sidebar.verticalTabs" = true;
+
+          # ArcWTF
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "svg.context-properties.content.enabled" = true;
+          "uc.tweak.popup-search" = true;
+          "uc.tweak.hide-sidebar-header" = true;
         };
 
         extensions = with osConfig.nur.repos.rycee.firefox-addons; [
@@ -37,7 +46,17 @@ in
           bitwarden
           search-by-image
           youtube-shorts-block
+          sidebery
+          userchrome-toggle
         ];
+      };
+    };
+
+    # https://github.com/KiKaraage/ArcWTF/issues/69
+    home.file = {
+      ".mozilla/firefox/${profile}/chrome" = {
+        source = arcwtf;
+        recursive = true;
       };
     };
   };
