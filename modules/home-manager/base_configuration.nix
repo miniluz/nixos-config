53c375f73969d1c478b3353ccffff1d8dfe009bf
miniluz <javiermelon4fu@gmail.com>
2025-07-03 15:17:31 +0200

    65 current  2025-07-03 15:17:31  25.11.20250607.3e3afe5  6.12.32                          *

--- modules/home-manager/base_configuration.nix
+++ modules/home-manager/base_configuration.nix
@@ -25,7 +25,7 @@
       "Rebuild" = {
         name = "Rebuild";
         genericName = "Rebuild NixOS";
-        exec = ''"rebuild \; echo \"Press enter to close this window...\" \; read ans"'';
+        exec = ''bash -c "rebuild ; echo \"Press enter to close this window...\" ; read ans"'';
         terminal = true;
       };
     };
