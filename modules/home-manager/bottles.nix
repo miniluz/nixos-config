922915341902ceb78550348afc79fb096007651a
miniluz <javiermelon4fu@gmail.com>
2025-07-04 22:31:27 +0200

    78 current  2025-07-04 22:31:26  25.11.20250607.3e3afe5  6.12.32                          *

--- modules/home-manager/bottles.nix
+++ modules/home-manager/bottles.nix
@@ -1,7 +1,7 @@
 {
   config,
   lib,
-  inputs,
+  pkgs,
   ...
 }:
 let
@@ -12,21 +12,13 @@ let
   #   rev = "3f4d52baad4347642a4ca6be68e21e6d41780469";
   #   sha256 = "sha256-cC/iXhjyf62zi5lqeBUtsq4VKxeHs74k6yMTDFiOnyo=";
   # };
-  pkgs = import inputs.nixpkgs {
-    system = "x86_64-linux";
-    overlays = [
-      (final: prev: {
-        wine = prev.wineWowPackages.stable;
-      })
-    ];
-  };
 in
 {
   options.miniluz.bottles.enable = lib.mkEnableOption "Enable bottles.";
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
-      wine
+      wineWowPackages.stable
       winetricks
       bottles
       yq
