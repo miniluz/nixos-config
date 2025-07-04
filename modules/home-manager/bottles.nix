be12a1f27cb7e9927f01e0d7477ede58b5342059
miniluz <javiermelon4fu@gmail.com>
2025-07-04 21:19:42 +0200

    75 current  2025-07-04 21:19:41  25.11.20250607.3e3afe5  6.12.32                          *

--- modules/home-manager/bottles.nix
+++ modules/home-manager/bottles.nix
@@ -6,12 +6,12 @@
 }:
 let
   cfg = config.miniluz.bottles;
-  yabridge-bottles-wineloader = pkgs.fetchFromGitHub {
-    owner = "microfortnight";
-    repo = "yabridge-bottles-wineloader";
-    rev = "3f4d52baad4347642a4ca6be68e21e6d41780469";
-    sha256 = "sha256-cC/iXhjyf62zi5lqeBUtsq4VKxeHs74k6yMTDFiOnyo=";
-  };
+  # yabridge-bottles-wineloader = pkgs.fetchFromGitHub {
+  #   owner = "microfortnight";
+  #   repo = "yabridge-bottles-wineloader";
+  #   rev = "3f4d52baad4347642a4ca6be68e21e6d41780469";
+  #   sha256 = "sha256-cC/iXhjyf62zi5lqeBUtsq4VKxeHs74k6yMTDFiOnyo=";
+  # };
 in
 {
   options.miniluz.bottles.enable = lib.mkEnableOption "Enable bottles.";
@@ -24,14 +24,14 @@ in
       yq
     ];
 
-    home.file.".local/bin/wineloader.sh" = {
-      source = "${yabridge-bottles-wineloader}/wineloader.sh";
-      executable = true;
-    };
+    # home.file.".local/bin/wineloader.sh" = {
+    #   source = "${yabridge-bottles-wineloader}/wineloader.sh";
+    #   executable = true;
+    # };
 
-    xdg.configFile."environment.d/wineloader.conf" = {
-      source = "${yabridge-bottles-wineloader}/wineloader.conf";
-    };
+    # xdg.configFile."environment.d/wineloader.conf" = {
+    #   source = "${yabridge-bottles-wineloader}/wineloader.conf";
+    # };
 
     home.sessionVariables."W_NO_WIN64_WARNINGS" = "1";
   };
