2d06a64a52e09d298cf9f6a384860280ec96372b
miniluz <javiermelon4fu@gmail.com>
2025-07-12 13:40:44 +0200

    142         2025-07-12 13:40:41  25.11.20250708.9807714  6.12.36  Unknown                 []              True

--- nvim/nvim-neotree.nix
+++ nvim/nvim-neotree.nix
@@ -1,6 +1,19 @@
+{ lib, ... }:
 {
   config.vim = {
-    filetree.neo-tree.enable = true;
+    filetree.neo-tree = {
+      enable = true;
+      setupOpts = {
+        event_handlers = {
+          event = "file_opened";
+          handler = lib.generators.mkLuaInline ''
+            function(file_path)
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          '';
+        };
+      };
+    };
     keymaps = [
       {
         key = "<leader>e";
