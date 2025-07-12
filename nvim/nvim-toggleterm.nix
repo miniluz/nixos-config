cf01ab8f79bf4e9fad1c36ee921aa98773c2f91f
miniluz <javiermelon4fu@gmail.com>
2025-07-12 13:48:34 +0200

    145         2025-07-12 13:48:31  25.11.20250708.9807714  6.12.36  Unknown                 []              True

--- nvim/nvim-toggleterm.nix
+++ nvim/nvim-toggleterm.nix
@@ -13,10 +13,17 @@
 
     keymaps = [
       {
+        key = "<C-'>";
+        mode = "t";
+        silent = true;
+        action = ''<C-\><C-n>'';
+        desc = "Exit terminal mode.";
+      }
+      {
         key = "<C-+>";
         mode = "n";
         silent = true;
-        action = '':1ToggleTerm | :1TermExec cmd="gitui"<cr>'';
+        action = '':1TermExec cmd="gitui"<cr>'';
         desc = "Toggle GitUI terminal.";
       }
       {
@@ -27,17 +34,10 @@
         desc = "Toggle GitUI terminal.";
       }
       {
-        key = "<C-'>";
-        mode = "t";
-        silent = true;
-        action = ''<C+\><C+n>'';
-        desc = "Exit terminal mode.";
-      }
-      {
         key = "<C-`>";
         mode = "n";
         silent = true;
-        action = '':2ToggleTerm | 2TermExec cmd="zellij"<cr>'';
+        action = ''2TermExec cmd="zellij"<cr>'';
         desc = "Exit terminal mode.";
       }
       {
