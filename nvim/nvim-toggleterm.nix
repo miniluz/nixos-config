bae12816e8503508be4411303be3eddc1eac4ef7
miniluz <javiermelon4fu@gmail.com>
2025-07-12 12:58:02 +0200

    135         2025-07-12 12:57:58  25.11.20250708.9807714  6.12.36  Unknown                 []              True

--- nvim/nvim-toggleterm.nix
+++ nvim/nvim-toggleterm.nix
@@ -3,10 +3,38 @@
   config.vim = {
     terminal.toggleterm = {
       enable = true;
+
+      mappings.open = "<C+`>";
+
       setupOpts = {
-        direction = "tab";
+        direction = "float";
       };
     };
+
+    keymaps = [
+      {
+        key = "<leader>gg";
+        mode = "n";
+        silent = true;
+        action = ''<cmd>1ToggleTerm | 1TermExec cmd="gitui"<cr>'';
+        desc = "Toggle GitUI terminal.";
+      }
+      {
+        key = "<C+º>";
+        mode = "t";
+        silent = true;
+        action = ''<C+\><C+n>'';
+        desc = "Exit terminal mode.";
+      }
+      {
+        key = "<C+`>";
+        mode = "t";
+        silent = true;
+        action = ''<cmd>ToggleTermToggleAll<cr>'';
+        desc = "Exit terminal mode.";
+      }
+
+    ];
 
   };
 
