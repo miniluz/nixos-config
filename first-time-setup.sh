65a2da2f4a2631c774459ca169a686108c19fbd0
miniluz <42033163+miniluz@users.noreply.github.com>
2025-08-08 15:42:58 +0200

    Update first-time-setup.sh

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -5,4 +5,4 @@ nix-shell -p git --command "cd ~/nixos-config; git remote set-url origin git@git
 echo -n "Enter host name: "
 read MACHINE_NAME
 
-nix-shell -p nix-output-monitor expect --command "sudo unbuffer nixos-rebuild swith -f ~/nixos-config/entry.nix -A nixosConfigurations.($MACHINE_NAME) --option extra-experimental-features 'nix-command flakes' --option max-jobs auto 2>&1 | nom"
+nix-shell -p nix-output-monitor expect --command "sudo unbuffer nixos-rebuild swith -f ~/nixos-config/entry.nix -A nixosConfigurations.$MACHINE_NAME --option extra-experimental-features 'nix-command flakes' --option max-jobs auto 2>&1 | nom"
