e6ced50332c237525b8b37b785fbba6d164bcf3c
miniluz <javiermelon4fu@gmail.com>
2025-08-08 18:41:53 +0200

    4 - 2025-08-08T16:41:52Z

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 nix-shell -p bitwarden-cli --command "bw login; bw get attachment id_ed25519 --itemid b49c4926-e119-4655-8db0-b19c008a438b --output ~/.ssh/id_ed25519"
 
 nix-shell -p git --command "cd ~/nixos-config; git remote set-url origin git@github.com:miniluz/nixos-config; git submodule init; git submodule update; cd private; git checkout main; git pull"
@@ -6,4 +8,4 @@ echo -n "Enter host name: "
 read MACHINE_NAME
 
 sudo -v
-nix-shell -p nix-output-monitor expect --command "sudo unbuffer nixos-rebuild swith -f ~/nixos-config/entry.nix -A nixosConfigurations.$MACHINE_NAME --option extra-experimental-features 'nix-command flakes' --option max-jobs auto 2>&1 | nom"
+nix-shell -p nix-output-monitor expect --command "sudo unbuffer nixos-rebuild switch -f ~/nixos-config/entry.nix -A nixosConfigurations.$MACHINE_NAME --option extra-experimental-features 'nix-command flakes' --option max-jobs auto 2>&1 | nom"
