d2dda47c199744b1f7a535d16ad419b38b9c2aee
miniluz <javiermelon4fu@gmail.com>
2025-09-04 21:16:21 +0200

    Server rebuild

--- modules/nixos/selfhosting/proxy/_homepage.nix
+++ modules/nixos/selfhosting/proxy/_homepage.nix
@@ -13,47 +13,6 @@ pkgs.writeTextFile {
       <head>
         <meta charset="UTF-8">
         <title>Home Server</title>
-        <script>
-          document.addEventListener('DOMContentLoaded', async () => {
-            async function findFavicon(url) {
-              try {
-                const u = new URL(url);
-                const origin = u.origin;
-                const icoUrl = origin + '/favicon.ico';
-                let res = await fetch(icoUrl, { method: 'HEAD' });
-                if (res.ok) return icoUrl;
-
-                res = await fetch(url);
-                if (!res.ok) return null;
-                const html = await res.text();
-
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-                const links = doc.querySelectorAll('link[rel~="icon"]');
-
-                for (const link of links) {
-                  let href = link.getAttribute('href');
-                  if (!href) continue;
-                  if (href.startsWith('/')) href = origin + href;
-                  else if (!/^https?:\/\//i.test(href)) href = origin + '/' + href;
-                  return href;
-                }
-
-                return null;
-              } catch (err) {
-                console.warn('Failed to fetch favicon for', url, err);
-                return null;
-              }
-            }
-
-            document.querySelectorAll('img.favicon').forEach(async (img) => {
-              const domain = img.dataset.domain;
-              const url = 'https://' + domain;
-              const favicon = await findFavicon(url);
-              if (favicon) img.src = favicon;
-            });
-          });
-        </script>
         <style>
           /* Catppuccin Mocha inspired dark theme */
           body {
@@ -107,7 +66,6 @@ pkgs.writeTextFile {
           ${lib.concatStringsSep "\n" (
             map (s: ''
               <li>
-                <img class="favicon" data-domain="${s.name}.${baseUrl}">
                 <a href="https://${s.name}.${baseUrl}" title="https://${s.name}.${baseUrl}">${s.name}</a>
               </li>
             '') proxies
