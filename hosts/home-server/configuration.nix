196ccefe80541fffb4c28a247f027de7adac1b9f
miniluz <javiermelon4fu@gmail.com>
2025-09-14 11:37:47 +0200

    Server rebuild

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
+{ pkgs, ... }:
 {
 
   miniluz.visual = false;
@@ -46,6 +46,24 @@
       ];
 
     };
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+      vpl-gpu-rt
+    ];
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
