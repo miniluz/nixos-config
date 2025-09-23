631a6f47cac13b6149d8505dc46cb9cf21a573b0
miniluz <javiermelon4fu@gmail.com>
2025-09-23 11:26:26 +0200

    293 - 2025-09-23T09:26:23Z

--- modules/nixos/browser/zen.nix
+++ modules/nixos/browser/zen.nix
@@ -7,39 +7,58 @@
 }:
 let
   cfg = config.miniluz;
+
+  extension = shortId: guid: {
+    name = guid;
+    value = {
+      install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+      installation_mode = "normal_installed";
+    };
+  };
+
+  prefs = {
+    "extensions.autoDisableScopes" = 0;
+    "extensions.pocket.enabled" = false;
+
+    "browser.toolbars.bookmarks.visibility" = "never";
+    "browser.tabs.closeWindowWithLastTab" = false; # don't close the window when closing the last tab
+
+    "signon.rememberSignons" = false;
+
+    "browser.theme.dark-private-windows" = true;
+    "zen.workspaces.separate-essentials" = false;
+  };
+
+  extensions = [
+    # To add additional extensions, find it on addons.mozilla.org, find
+    # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+    # Then go to https://addons.mozilla.org/api/v5/addons/addon/!SHORT_ID!/ to get the guid
+    (extension "ublock-origin" "uBlock0@raymondhill.net")
+    (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+    (extension "search_by_image" "{2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c}")
+    (extension "youtube-shorts-block" "{34daeb50-c2d2-4f14-886a-7160b24d66a4}")
+    (extension "istilldontcareaboutcookies" "idcac-pub@guus.ninja")
+    # (extension "sidebery" "{3c078156-979c-498b-8990-85f7987dd929}")
+    # (extension "vimium-ff" "{d7742d87-e61d-4b78-b8a1-b469842139fa}")
+    (extension "catppuccin-web-file-icons" "{bbb880ce-43c9-47ae-b746-c3e0096c5b76}")
+    (extension "untrap-for-youtube" "{2662ff67-b302-4363-95f3-b050218bd72c}")
+    (extension "wayback-machine_new" "wayback_machine@mozilla.org")
+  ];
+
 in
 {
   config = lib.mkIf (cfg.firefox.enable && cfg.visual) {
     environment.systemPackages = [
       (pkgs.wrapFirefox inputs.zen-browser.packages.${pkgs.system}.zen-browser-unwrapped {
+        extraPrefs = lib.concatLines (
+          lib.mapAttrsToList (
+            name: value: ''lockPref(${lib.strings.toJSON name}, ${lib.strings.toJSON value});''
+          ) prefs
+        );
+
         extraPolicies = {
           DisableTelemetry = true;
-          ExtensionSettings =
-            with builtins;
-            let
-              extension = shortId: guid: {
-                name = guid;
-                value = {
-                  install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
-                  installation_mode = "normal_installed";
-                };
-              };
-            in
-            listToAttrs [
-              # To add additional extensions, find it on addons.mozilla.org, find
-              # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
-              # Then go to https://addons.mozilla.org/api/v5/addons/addon/!SHORT_ID!/ to get the guid
-              (extension "ublock-origin" "uBlock0@raymondhill.net")
-              (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
-              (extension "search_by_image" "{2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c}")
-              (extension "youtube-shorts-block" "{34daeb50-c2d2-4f14-886a-7160b24d66a4}")
-              (extension "istilldontcareaboutcookies" "idcac-pub@guus.ninja")
-              # (extension "sidebery" "{3c078156-979c-498b-8990-85f7987dd929}")
-              # (extension "vimium-ff" "{d7742d87-e61d-4b78-b8a1-b469842139fa}")
-              (extension "catppuccin-web-file-icons" "{bbb880ce-43c9-47ae-b746-c3e0096c5b76}")
-              (extension "untrap-for-youtube" "{2662ff67-b302-4363-95f3-b050218bd72c}")
-              (extension "wayback-machine_new" "wayback_machine@mozilla.org")
-            ];
+          ExtensionSettings = builtins.listToAttrs extensions;
 
           SearchEngines = {
             Default = "ddg";
@@ -93,20 +112,6 @@ in
                 Alias = "@rs";
               }
             ];
-          };
-
-          Preferences = {
-            "extensions.autoDisableScopes" = 0;
-            "extensions.pocket.enabled" = false;
-
-            "browser.toolbars.bookmarks.visibility" = "never";
-            "browser.tabs.closeWindowWithLastTab" = false; # don't close the window when closing the last tab
-
-            "signon.rememberSignons" = false;
-
-            #"toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-            "browser.theme.dark-private-windows" = true;
-            "extensions.activeThemeID" = "firefox-dark@mozilla.org";
           };
         };
       })
