e26f0eb5cb4ed07ed4dfda5142d573e60d332ae3
miniluz <javiermelon4fu@gmail.com>
2025-09-30 15:33:59 +0200

    118 - 2025-09-30T13:32:02Z

--- modules/nixos/core/audio.nix
+++ modules/nixos/core/audio.nix
@@ -7,6 +7,84 @@
 let
   cfg = config.miniluz.audio;
 in
+# To find device configs, run:
+# nix-shell -p pulseaudio --run "pactl list sources" | rg Name
+# nix-shell -p pulseaudio --run "pactl list sinks" | rg Name
+# Example configs:
+# services.pipewire = {
+#     extraConfig.pipewire."90-scarlett-defaults" = {
+#       "context.properties" = {
+#         # Set Scarlett Line 1-2 as default output
+#         "default.audio.sink" = "alsa_output.usb-Focusrite_Scarlett_2i4_USB-00.HiFi__Line1__sink";
+#         # Set Scarlett Input 1 as default input
+#         "default.audio.source" = "alsa_input.usb-Focusrite_Scarlett_2i4_USB-00.HiFi__Mic1__source";
+#       };
+#     };
+#     wireplumber.extraConfig."51-scarlett-priority" = {
+#       "monitor.alsa.rules" = [
+#         # Disable HDMI audio
+#         {
+#           matches = [
+#             { "node.name" = "alsa_output.pci-0000_0c_00.1.hdmi-stereo"; }
+#           ];
+#           actions = {
+#             update-props = {
+#               "node.disabled" = true;
+#             };
+#           };
+#         }
+#         {
+#           matches = [
+#             { "node.name" = "alsa_input.usb-046d_HD_Pro_Webcam_C920_E4F1F8DF-02.analog-stereo"; }
+#           ];
+#           actions = {
+#             update-props = {
+#               "node.disabled" = true;
+#             };
+#           };
+#         }
+#
+#         # Set high priority for Scarlett outputs
+#         {
+#           matches = [
+#             { "node.name" = "~alsa_output.usb-Focusrite_Scarlett_2i4_USB.*"; }
+#           ];
+#           actions = {
+#             update-props = {
+#               "priority.driver" = 1000;
+#               "priority.session" = 1000;
+#             };
+#           };
+#         }
+#
+#         # Set high priority for Scarlett inputs
+#         {
+#           matches = [
+#             { "node.name" = "~alsa_input.usb-Focusrite_Scarlett_2i4_USB.*"; }
+#           ];
+#           actions = {
+#             update-props = {
+#               "priority.driver" = 1000;
+#               "priority.session" = 1000;
+#             };
+#           };
+#         }
+#
+#         # Lower priority for onboard audio
+#         {
+#           matches = [
+#             { "node.name" = "~alsa_.*pci-0000_0e_00.4.*"; }
+#           ];
+#           actions = {
+#             update-props = {
+#               "priority.driver" = 100;
+#               "priority.session" = 100;
+#             };
+#           };
+#         }
+#       ];
+#     };
+#   };
 {
   imports = [
     inputs.musnix.nixosModules.musnix
