8bbaac77a404c325de9f6b3cf11f61f96a181269
miniluz <javiermelon4fu@gmail.com>
2025-10-17 10:55:51 +0200

    144 - 2025-10-17T08:55:47Z

--- hosts/sunlight/configuration.nix
+++ hosts/sunlight/configuration.nix
@@ -1,6 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
+{ pkgs, lib, ... }:
 {
   miniluz.gnome.enable = true;
   miniluz.audio = {
@@ -43,87 +44,76 @@
         "default.audio.source" = "alsa_input.usb-Focusrite_Scarlett_2i4_USB-00.HiFi__Mic1__source";
       };
     };
-    wireplumber.extraConfig = {
-      "51-scarlett-priority" = {
-        "monitor.alsa.rules" = [
-          # Disable HDMI audio
-          {
-            matches = [
-              { "node.name" = "alsa_output.pci-0000_0c_00.1.hdmi-stereo"; }
-            ];
-            actions = {
-              update-props = {
-                "node.disabled" = true;
-              };
-            };
-          }
-          {
-            matches = [
-              { "node.name" = "alsa_input.usb-046d_HD_Pro_Webcam_C920_E4F1F8DF-02.analog-stereo"; }
-            ];
-            actions = {
-              update-props = {
-                "node.disabled" = true;
-              };
-            };
-          }
-
-          # Set high priority for Scarlett outputs
-          {
-            matches = [
-              { "node.name" = "~alsa_output.usb-Focusrite_Scarlett_2i4_USB.*"; }
-            ];
-            actions = {
-              update-props = {
-                "priority.driver" = 1000;
-                "priority.session" = 1000;
-              };
-            };
-          }
-
-          # Set high priority for Scarlett inputs
-          {
-            matches = [
-              { "node.name" = "~alsa_input.usb-Focusrite_Scarlett_2i4_USB.*"; }
-            ];
-            actions = {
-              update-props = {
-                "priority.driver" = 1000;
-                "priority.session" = 1000;
-              };
-            };
-          }
-
-          # Lower priority for onboard audio
-          {
-            matches = [
-              { "node.name" = "~alsa_.*pci-0000_0e_00.4.*"; }
-            ];
-            actions = {
-              update-props = {
-                "priority.driver" = 100;
-                "priority.session" = 100;
-              };
-            };
-          }
-        ];
-        "51-hdmi-surround" = {
-          "monitor.alsa.rules" = [
-            {
-              matches = [
-                { "device.name" = "alsa_card.pci-0000_0c_00.1"; }
-              ];
-              actions = {
-                update-props = {
-                  "device.profile" = "output:hdmi-surround-extra3";
-                };
-              };
-            }
+    wireplumber.extraConfig."51-scarlett-priority" = {
+      "monitor.alsa.rules" = [
+        # Disable HDMI audio
+        {
+          matches = [
+            { "node.name" = "alsa_output.pci-0000_0c_00.1.hdmi-stereo"; }
           ];
-        };
-      };
+          actions = {
+            update-props = {
+              "node.disabled" = true;
+            };
+          };
+        }
+        {
+          matches = [
+            { "node.name" = "alsa_input.usb-046d_HD_Pro_Webcam_C920_E4F1F8DF-02.analog-stereo"; }
+          ];
+          actions = {
+            update-props = {
+              "node.disabled" = true;
+            };
+          };
+        }
+
+        # Set high priority for Scarlett outputs
+        {
+          matches = [
+            { "node.name" = "~alsa_output.usb-Focusrite_Scarlett_2i4_USB.*"; }
+          ];
+          actions = {
+            update-props = {
+              "priority.driver" = 1000;
+              "priority.session" = 1000;
+            };
+          };
+        }
+
+        # Set high priority for Scarlett inputs
+        {
+          matches = [
+            { "node.name" = "~alsa_input.usb-Focusrite_Scarlett_2i4_USB.*"; }
+          ];
+          actions = {
+            update-props = {
+              "priority.driver" = 1000;
+              "priority.session" = 1000;
+            };
+          };
+        }
+
+        # Lower priority for onboard audio
+        {
+          matches = [
+            { "node.name" = "~alsa_.*pci-0000_0e_00.4.*"; }
+          ];
+          actions = {
+            update-props = {
+              "priority.driver" = 100;
+              "priority.session" = 100;
+            };
+          };
+        }
+      ];
     };
   };
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "switch-to-surround" "${lib.getExe' pkgs.pulseaudio "pactl"} set-card-profile alsa_card.pci-0000_0c_00.1 output:hdmi-surround-extra3")
+    (pkgs.writeShellScriptBin "switch-to-stereo" "${lib.getExe' pkgs.pulseaudio "pactl"} set-card-profile alsa_card.pci-0000_0c_00.1 output:hdmi-stereo-extra3")
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
