d6f6abc5436c09df31c8eb89228d5ebd4058a047
miniluz <javiermelon4fu@gmail.com>
2025-10-26 19:58:20 +0100

    159 - 2025-10-26 19:58:18

--- modules/nixos/core/visual/gnome/gnome.nix
+++ modules/nixos/core/visual/gnome/gnome.nix
@@ -72,6 +72,13 @@ in
               night-light-temperature = lib.gvariant.mkUint32 1700;
             };
 
+            # TODO: make this extensible
+            "org/gnome/settings-daemon/plugins/media-keys" = {
+              "custom-keybindings" = [
+                "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+              ];
+            };
+
             "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
               binding = "<Super>k";
               command = "kitty";
