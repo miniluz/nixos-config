1ce2f9eee80224407e8e318792f75f87e7730a9f
miniluz <javiermelon4fu@gmail.com>
2025-11-03 21:17:40 +0100

    347 - 2025-11-03 21:17:38

--- pkgs/shell/ghostty-luzwrap.nix
+++ pkgs/shell/ghostty-luzwrap.nix
@@ -24,6 +24,37 @@ symlinkJoin {
     ln -sf ${./ghostty-config.conf} $out/ghostty/config
     ln -sf ${catppuccin-ghostty}/themes $out/ghostty/themes
 
+    rm $out/share/applications/com.mitchellh.ghostty.desktop
+
+    cat > $out/share/applications/com.mitchellh.ghostty.desktop <<INI
+      [Desktop Entry]
+      Version=1.0
+      Name=Ghostty
+      Type=Application
+      Comment=A terminal emulator
+      TryExec=$out/bin/ghostty
+      Exec=$out/bin/ghostty --gtk-single-instance=true
+      Icon=com.mitchellh.ghostty
+      Categories=System;TerminalEmulator;
+      Keywords=terminal;tty;pty;
+      StartupNotify=true
+      StartupWMClass=com.mitchellh.ghostty
+      Terminal=false
+      Actions=new-window;
+      X-GNOME-UsesNotifications=true
+      X-TerminalArgExec=-e
+      X-TerminalArgTitle=--title=
+      X-TerminalArgAppId=--class=
+      X-TerminalArgDir=--working-directory=
+      X-TerminalArgHold=--wait-after-command
+      DBusActivatable=true
+      X-KDE-Shortcuts=Ctrl+Alt+T
+
+      [Desktop Action new-window]
+      Name=New Window
+      Exec=$out/bin/ghostty --gtk-single-instance=true
+    INI
+
     wrapProgram $out/bin/ghostty \
       --add-flag "--config-file=$out/ghostty/config" \
       --add-flag "--theme=$out/ghostty/themes/catppuccin-mocha.conf"
