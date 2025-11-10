d53ba46258184bac7ff2cc65e0bde47c97c00257
miniluz <javiermelon4fu@gmail.com>
2025-11-10 10:34:49 +0100

    362 - 2025-11-10 10:34:47

--- pkgs/scripts/git-clean-branches.nix
+++ pkgs/scripts/git-clean-branches.nix
@@ -1,4 +1,4 @@
 { writeShellScriptBin }:
 writeShellScriptBin "git-clean-branches" ''
-  git for-each-ref --format '%(refname:short)' refs/heads | grep -v "master\|main\|develop\|development\|dev" | xargs git branch -D
+  git for-each-ref --format '%(refname:short)' refs/heads | grep -v "master\|main\|develop\|development\|dev\|trunk" | xargs git branch -D
 ''
