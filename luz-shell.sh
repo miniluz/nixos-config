759745e7635b4cbb78643d8a95e52997fc68070b
miniluz <42033163+miniluz@users.noreply.github.com>
2025-11-11 16:11:20 +0100

    Implement fish shell execution in luz-shell.sh
    Add fish shell execution condition to luz-shell.sh

--- luz-shell.sh
+++ luz-shell.sh
@@ -5,3 +5,10 @@ nix shell github:miniluz/nixos-config#miniluz-pkgs.luz-shell-utils
 nix run github:miniluz/nixos-config#miniluz-pkgs.luz-shell
 
 nix profile install github:miniluz/nixos-config#miniluz-pkgs.luz-shell --extra-experimental-features nix-command --extra-experimental-features flakes
+
+# You'll also wanna add this to the .bashrc
+if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ${BASH_EXECUTION_STRING} ]]
+    then
+    shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+    exec fish $LOGIN_OPTION
+fi
