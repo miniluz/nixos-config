5a3253f394f6300be2adf0c5478cf50db2c68bca
miniluz <javiermelon4fu@gmail.com>
2026-03-31 13:00:41 +0200

    456 - 2026-03-31 13:00:39

--- modules/nixos/productivity/development/nvim/nvim.nix
+++ modules/nixos/productivity/development/nvim/nvim.nix
@@ -7,18 +7,12 @@
 }:
 let
   cfg = config.miniluz.development;
-  secretDir = "/run/user/${builtins.toString config.users.users.miniluz.uid}/agenix";
 in
 {
   config = lib.mkIf (cfg.enable && cfg.nvim.enable) {
 
-    systemd.tmpfiles.rules = [
-      "d ${secretDir} 700 miniluz users"
-    ];
-
     age.secrets.opencode-env = {
       file = "${global-secrets}/opencode-env.age";
-      path = "${secretDir}/opencode-env";
       mode = "700";
       owner = "miniluz";
       group = "users";
