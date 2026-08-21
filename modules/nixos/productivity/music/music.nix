0c70b899fbf4f6eaec22ef7dba569a65934dc54c
miniluz <miniluz@miniluz.dev>
2026-08-21 09:52:36 +0200

    206 - 2026-08-21 09:40:43

--- modules/nixos/productivity/music/music.nix
+++ modules/nixos/productivity/music/music.nix
@@ -12,7 +12,8 @@ in
 
   config = lib.mkIf cfg.enable {
     users.users.miniluz.packages = with pkgs; [
-      (reaper.overrideAttrs { jackLibrary = pipewire.jack; })
+      reaper
+      # (reaper.overrideAttrs { jackLibrary = pipewire.jack; })
       reaper-reapack-extension
 
       helm
