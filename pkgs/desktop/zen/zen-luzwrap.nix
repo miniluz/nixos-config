e48d0145f6f7b56d5f5eb7a0e4dd55de4de4dc9c
miniluz <miniluz@miniluz.dev>
2026-09-11 12:38:04 +0200

    252 - 2026-09-11 12:38:00

--- pkgs/desktop/zen/zen-luzwrap.nix
+++ pkgs/desktop/zen/zen-luzwrap.nix
@@ -9,17 +9,24 @@ let
   extensions = import ./_extensions.nix;
   SearchEngines = import ./_search-engines.nix;
 in
-wrapFirefox zen-browser-unwrapped {
-  extraPrefs = lib.concatLines (
-    lib.mapAttrsToList (
-      name: value: "lockPref(${lib.strings.toJSON name}, ${lib.strings.toJSON value});"
-    ) prefs
-  );
+wrapFirefox
+  (zen-browser-unwrapped.overrideAttrs (old: {
+    passthru = (old.passthru or { }) // {
+      withFFmpeg = true;
+      withGSSAPI = true;
+    };
+  }))
+  {
+    extraPrefs = lib.concatLines (
+      lib.mapAttrsToList (
+        name: value: "lockPref(${lib.strings.toJSON name}, ${lib.strings.toJSON value});"
+      ) prefs
+    );
 
-  extraPolicies = {
-    DisableTelemetry = true;
-    ExtensionSettings = builtins.listToAttrs extensions;
+    extraPolicies = {
+      DisableTelemetry = true;
+      ExtensionSettings = builtins.listToAttrs extensions;
 
-    inherit SearchEngines;
-  };
-}
+      inherit SearchEngines;
+    };
+  }
